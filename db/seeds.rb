72cfcf4440a6fe5ae814c9eb79cc18d193561ea9
Joseph Fulkerson <josephafulkerson@gmail.com>
2021-11-22 14:44:32 -0500

    final push

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,27 +7,27 @@
 
 
 # def legislator_set
-#     states = [ 'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'ND', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY' ]
-#     api_data = {key: open_secrets_key}
-#     states.each do |state|
-#     legislators = RestClient.get("http://www.opensecrets.org/api/?method=getLegislators&id=#{state}&apikey=#{api_data[:key]}")
-#     hash = Hash.from_xml(legislators)
-#     # puts hash["response"]["legislator"]
-#     # puts "#{state} + ***************"
-#     # legislator_array = JSON.parse(legislators)
-#     hash["response"]["legislator"].each do |l|
-#         puts l["firstlast"]
-#        current_l = Legislator.create(name: l["firstlast"], party: l["party"], website_url: l["website"])
-#         data = RestClient.get("https://www.opensecrets.org/api/?method=candContrib&cid=#{l["cid"]}&cycle=2020&apikey=#{api_data[:key]}")
-#         contributions = Hash.from_xml(data)
-#         # byebug
-#         contributions["response"]["contributors"]["contributor"].each do |cln| 
-#             Contribution.create(organization: cln["org_name"], amount: cln["total"].to_i, legislator_id: current_l.id )
-#         end
+    states = [ 'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'ND', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY' ]
+    api_data = {key: open_secrets_key}
+    states.each do |state|
+    legislators = RestClient.get("http://www.opensecrets.org/api/?method=getLegislators&id=#{state}&apikey=#{api_data[:key]}")
+    hash = Hash.from_xml(legislators)
+    # puts hash["response"]["legislator"]
+    # puts "#{state} + ***************"
+    # legislator_array = JSON.parse(legislators)
+    hash["response"]["legislator"].each do |l|
+        puts l["firstlast"]
+       current_l = Legislator.create(name: l["firstlast"], party: l["party"], website_url: l["website"])
+        data = RestClient.get("https://www.opensecrets.org/api/?method=candContrib&cid=#{l["cid"]}&cycle=2020&apikey=#{api_data[:key]}")
+        contributions = Hash.from_xml(data)
+        # byebug
+        contributions["response"]["contributors"]["contributor"].each do |cln| 
+            Contribution.create(organization: cln["org_name"], amount: cln["total"].to_i, legislator_id: current_l.id )
+        end
 
-#         end
-#     end
-# end
+        end
+    end
+end
 
 # legislator_set()
 # legislator = Legislator.create(name: 'Gorp', party: 'republican', website_url: "google.com")
